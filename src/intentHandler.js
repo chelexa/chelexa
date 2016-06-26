94a2284b238b7411790450209b0ea883d6efbf42
Alex Shorsher <alex.shorsher@gmail.com>
2016-06-26 00:30:53 -0700

    Defined intents in intentHandler.js

--- src/intentHandler.js
+++ src/intentHandler.js
@@ -1,1 +1,24 @@
 'use strict';
+var textHelper = require('./textHelper'),
+    storage = require('./storage');
+
+var registerIntentHandlers = function (intentHandlers, skillContext) {
+  intentHandlers.NewGameIntent = function (intent, session, response) {
+    //starting a new game
+    storage.loadGame(session, function (currentGame) {
+      if (currentGame.data.players.length === 0) {
+        response.ask('New game started. Choose your color please.');
+        return;
+      }
+      //Start the game with no moves
+      currentGame.data = '';
+    });
+  };
+
+  intentHandlers.PlayMove = function (intent, session, response) {
+
+
+
+  };
+
+}
