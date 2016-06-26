2a027d5c0931c2d582b74ede643a4c9707353a8d
Alex Shorsher <alex.shorsher@gmail.com>
2016-06-26 01:34:55 -0700

    Added chessMap and playMove intent functionality

---
+++ src/chessMap.js
@@ -0,0 +1,19 @@
+var pieceMap = {
+  "rook" : "R",
+  "bishop" : "B",
+  "knight" : "Q",
+  "queen" : "N",
+  "king" : "K",
+  "pawn" : ""
+};
+
+
+var actionsMap = {
+  "to" : "",
+  "takes" : "x",
+  "captures" : "x",
+  "takes on" : "x",
+  "captures on" : "x",
+  //Null takes care of value null :)
+  "null" : ""
+};
--- src/intentHandler.js
+++ src/intentHandler.js
@@ -1,22 +1,57 @@
 'use strict';
 var textHelper = require('./textHelper'),
-    storage = require('./storage');
+    storage = require('./storage'),
+    map = require('./chessMap');
+
+var versusAI = true;
 
 var registerIntentHandlers = function (intentHandlers, skillContext) {
   intentHandlers.NewGameIntent = function (intent, session, response) {
     //starting a new game
     storage.loadGame(session, function (currentGame) {
       if (currentGame.data.players.length === 0) {
-        response.ask('New game started. Choose your color please.');
+        response.ask('New game started. You are the white player.');
         return;
       }
-      //Start the game with no moves
+      //Start the game with no moves saved
       currentGame.data = '';
     });
   };
 
   intentHandlers.PlayMove = function (intent, session, response) {
 
+    //give a player points, ask additional question if slot values are missing.
+    var piece = intent.slots.Piece.value,
+        file = intent.slots.File.value,
+        rank = intent.slots.Rank.value,
+        action = intent.slots.Action.value,
+        move;
+
+    if (map.pieceMap[piece] !== null) {
+        piece = map.pieceMap[piece];
+    }
+
+    action = map.actionsMap[action];
+
+    //formatting our response
+    move = piece + action + file + rank
+
+    response = // TODO - Call Tyler here
+
+    if (response /* ERROR */ ) {
+      response.ask('sorry, not a valid move, please choose again');
+      return;
+    }
+
+    if ( versusAI ) {
+      //TODO: fill this shit out
+    } else {
+      currentGame.data.fen = //TODO: Response fen
+      storage.save( function () {
+        //TODO: maybe indicate color of next move here
+        response.ask('next move');
+      });
+    }
   };
 
   intentHandlers.PlayMoveSpecifyFile = function (intent, session, response) {
