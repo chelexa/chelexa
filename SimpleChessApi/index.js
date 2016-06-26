881419d40e135626175c10caee45e1b03e06e509
tyzeller <tyzeller@cisco.com>
2016-06-26 04:51:24 -0700

    change port

--- SimpleChessApi/index.js
+++ SimpleChessApi/index.js
@@ -7,6 +7,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
  
 var routes = require("./routes/routes.js")(app);
  
-var server = app.listen(3000, function () {
+var server = app.listen(8000, function () {
     console.log("Listening on port %s...", server.address().port);
 });
